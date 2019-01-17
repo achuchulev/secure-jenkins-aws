3d2d6c95a88270477c448f72f2217ef6511fc173
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-01-17 16:55:39 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "public_key" {}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "ami" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+
+variable "vpc_security_group_ids" {
+  type = "list"
+}
