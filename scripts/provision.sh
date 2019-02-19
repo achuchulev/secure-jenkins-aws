4d89331bb318c9dbd63db48ee9dbdaf9e1fd980c
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-02-19 11:53:41 +0200

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Make sure apt repository db is up to date
-sudo apt-get update
+sudo apt update
 
 # Install tools
 echo "Installing tools...."
-which wget curl telnet unzip &>/dev/null || {
-  sudo apt-get install -y wget curl telnet unzip
-}
+sudo apt install -y wget curl telnet unzip
 
 # Check if nginx is installed
 # Install nginx if not installed
 echo "Installing nginx...."
 which nginx &>/dev/null || {
-  sudo apt-get install -y nginx
+  sudo apt install -y nginx
 }
 
 # Configure nginx
@@ -38,7 +36,7 @@ sudo systemctl start nginx.service
 # Install java-jdk required for jenkins to run
 echo "Installing Java JDK...."
 which java &>/dev/null || {
-  sudo apt-get install -y default-jdk
+  sudo apt install -y default-jdk
   which java
   type -p java
 }
@@ -48,8 +46,8 @@ echo "Installing Jenkins...."
 which jenkins &>/dev/null || {
   wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
   sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-  sudo apt-get update
-  sudo apt-get install -y jenkins
+  sudo apt update
+  sudo apt install -y jenkins
 }
 
 sleep 10
@@ -59,11 +57,11 @@ sudo systemctl start jenkins.service
 
 # Install EFF's Certbot tool
 echo "Installing Certbot...."
-sudo apt-get install software-properties-common -y
+sudo apt install software-properties-common -y
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:certbot/certbot -y
-sudo apt-get update
-sudo apt-get install python-certbot-nginx -y
+sudo apt update
+sudo apt install python-certbot-nginx -y
 
 # Create cron job to check and renew certificate on expiration
 crontab <<EOF
